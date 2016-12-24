be473a6c6969d6405c7bbe34034039925099051b
hezj <eric6356@gmail.com>
2016-12-24 10:50:24 +0800

    background done

--- background.js
+++ background.js
@@ -1,35 +1,42 @@
-var showRule = {
-  // That fires when a page's URL contains a 'g' ...
-  conditions: [
-    new chrome.declarativeContent.PageStateMatcher({
-      pageUrl: { urlPrefix: 'http://ipn.li/' },
-    })
-  ],
-  // And shows the extension's page action.
-  actions: [new chrome.declarativeContent.ShowPageAction()]
+var ipnIconDict = {
+  '壁下观': 'icon-bixiaguan-300.png',
+  '博物志': 'icon-bowuzhi-300.png',
+  '疯投圈': 'icon-crazycapital-300.png',
+  '时尚怪物': 'icon-fashionmonster-300.png',
+  '硬影像': 'icon-hardimage-300.png',
+  '(Hi)story': 'icon-history-300.png',
+  'IT公论': 'icon-itgonglun-300.png',
+  '内核恐慌': 'icon-kernelpanic-300.png',
+  '流行通信': 'icon-popdispatch-300.png',
+  '太医来了': 'icon-taiyilaile-300.png',
+  '無次元': 'icon-wcy-300.jpg',
+  '味之道': 'icon-weizhidao-300.png',
+  '选美': 'icon-xuanmei-300.png',
+  '一天世界': 'icon-yitianshijie-300.png',
 }
 
-// When the extension is installed or upgraded ...
 chrome.runtime.onInstalled.addListener(function () {
   var itemQueue = new Array(20)
-  chrome.storage.sync.set({itemQueue}, function () {console.log('itemQueue initilized.')})
-
-  // Replace all rules ...
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    // With a new rule ...
-    chrome.declarativeContent.onPageChanged.addRules([showRule]);
-  });
-});
+  var subscribed = ['味之道', '流行通信', '壁下观看', '疯投圈', '太医来了', '内核恐慌', '时尚怪物', '一天世界', '硬影像', '博物志', '無次元']
+  chrome.storage.sync.set({ itemQueue, subscribed }, function () { console.log('itemQueue initilized.') })
+})
 
 /*
 itemQueue: [oldestItem, secondOldestItem, ... latestItem]
  */
-
 function parseItem(itemEL) {
   var item = {}
   for (var i = 0; i < itemEL.children.length; i++) {
-    childEL = itemEL.children[i]
-    item[childEL.tagName] = childEL.innerHTML
+    var childEL = itemEL.children[i]
+    if (childEL.tagName === 'description') {
+      item.description = childEL.textContent.trim()
+      var el = document.createElement('div')
+      el.innerHTML = item.description
+      item.descriptionText = el.innerText
+    }
+    else {
+      item[childEL.tagName] = childEL.innerHTML
+    }
   }
   var splitTitle = item.title.split(' ')
   item.podcast = splitTitle[0]
@@ -42,7 +49,7 @@ function parseItem(itemEL) {
 function isNewItem(item) {
   console.log(`judging: ${item.title}`)
   return new Promise(function (resolve, reject) {
-    chrome.storage.sync.get({itemQueue: []}, function (x) {
+    chrome.storage.sync.get({ itemQueue: [] }, function (x) {
       // TODO: use binary search here
       for (var i = 0; i < x.itemQueue.length; i++) {
         var storedItem = x.itemQueue[i]
@@ -52,7 +59,7 @@ function isNewItem(item) {
         }
         if (item.title === storedItem.title) {
           console.log(`same as storedItem[${i}], break`)
-          resolve({isNew: false, indexInQueue: i})
+          resolve({ isNew: false, indexInQueue: i, storedItem: storedItem })
           return
         }
         else {
@@ -60,7 +67,7 @@ function isNewItem(item) {
         }
       }
       console.log(`item is new`)
-      resolve({isNew: true})
+      resolve({ isNew: true })
     })
   })
 }
@@ -68,22 +75,49 @@ function isNewItem(item) {
 function pushItems(items) {
   // items: [latestItem, secondLatestItem, ... oldestItem]
   return new Promise(function (resolve, reject) {
-    chrome.storage.sync.get({itemQueue: []}, function (x) {
+    chrome.storage.sync.get('itemQueue', function (x) {
       items.slice().reverse().map(function (item) {
-        delete item.description
         x.itemQueue.shift()
-        x.itemQueue.push(item)
+        x.itemQueue.push({
+          title: item.title,
+          pubDate: item.pubDate,
+          link: item.link,
+          read: false
+        })
         console.log(`item pushed: ${item.title}`)
       })
-      chrome.storage.sync.set({itemQueue: x.itemQueue}, function () {
+      chrome.storage.sync.set({ itemQueue: x.itemQueue }, function () {
         resolve(items)
       })
     })
   })
 }
 
+function notifyItemIfSubscribed(item) {
+  return new Promise(function (resolve, reject) {
+    chrome.storage.sync.get('subscribed', function (x) {
+      if (x.subscribed.indexOf(item.podcast) !== -1) {
+        var message = item.descriptionText.split('\n')[0]
+        if (message.length > 20) {
+          message = message.slice(0, 17) + '...'
+        }
+        var opt = {
+          type: 'basic',
+          iconUrl: 'img/ipn_icons/' + ipnIconDict[item.podcast],
+          title: '#' + item.episode + ' ' + item.pureTitle,
+          message: message,
+          isClickable: true
+        }
+        chrome.notifications.create(item.title, opt, function () {
+          resolve(item)
+        })
+      }
+    })
+  })
+}
+
+var items
 function fetchAndUpdate() {
-  var items
   var parser = new DOMParser();
   fetch('http://ipn.li/feed')
     .then(function (response) {
@@ -110,14 +144,31 @@ function fetchAndUpdate() {
     })
     .then(function (items) {
       if (items !== undefined) {
-        // TODO: handle notification here
-        (function () {})()
+        return Promise.all(items.map(notifyItemIfSubscribed))
       }
     })
 }
 
-chrome.alarms.create('fetch', { periodInMinutes: 10 })
+chrome.notifications.onClicked.addListener(function (title) {
+  console.log(title)
+  isNewItem({ title: title })
+    .then(function (result) {
+      window.open(result.storedItem.link, '_blank')
+      chrome.notifications.clear(title)
+    })
+})
+
+chrome.storage.sync.get({ notiEnabled: false }, function (x) {
+  if (x.notiEnabled) {
+    console.log(`fetch and update for the first time at ${new Date()}`)
+    fetchAndUpdate()
+    chrome.alarms.create('fetch', { periodInMinutes: 10 })
+  }
+})
 
 chrome.alarms.onAlarm.addListener(function (alarm) {
-  fetchAndUpdate()
+  if (alarm.name === 'fetch') {
+    console.log(`fetch and update at ${new Date()}`)
+    fetchAndUpdate()
+  }
 })
